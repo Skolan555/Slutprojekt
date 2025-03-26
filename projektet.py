93033fd481138a297204ec8b7a33fba7b2d5fd12
Skolan555 <alex.vestermark@elev.ga.ntig.se>
2025-03-26 13:35:53 +0100

    Jag hämta api och kolla hur api se ut

--- projektet.py
+++ projektet.py
@@ -1,9 +1,35 @@
-print("hallo")
+import requests
+
+def api_products():
+    url = "https://dummyjson.com/products"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        data = response.json()
+        products = data.get("products", [])
+        
+        for product in products[:]:  # visa antal produkter
+            print(f"Namn: {product['title']}")
+            print(f"Pris: {product['price']} USD")
+            print(f"Beskrivning: {product['description']}")
+            print(f"Bild: {product['thumbnail']}")
+            print("-" * 40) 
+    else:
+        print("Misslyckades att hämta produkter")
 
 
+
+def main():
+    api_products()
+    
+    
+main()
 
 """
 git add .
 git commit -m "First py file"
 git push
+
+194 id, 24 category
 """
+
