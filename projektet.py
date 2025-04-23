8e266a1c93f30cf96db977ba3a526a1bcc1f3381
Skolan555 <alex.vestermark@elev.ga.ntig.se>
2025-04-23 13:21:53 +0200

    Jag planerar mest på vad man ska ha klasser och kodstruktur. Det kommer fler klasser senare.

--- projektet.py
+++ projektet.py
@@ -1,5 +1,35 @@
 import requests
 
+saldo = 0
+
+class konto():
+    def __init__(self, namn, email, lösenord, saldo):
+        self._namn = namn
+        self.__email = email
+        self.__lösenord = lösenord
+        self.__saldo = saldo
+        
+    def set_email(self, email):
+        self.__email = email
+    
+    def get_email(self):
+        return self.__email
+    
+    def set_lösenord(self, lösenord):
+        self.__lösenord = lösenord
+    
+    def get_lösenordl(self):
+        return self.__lösenord
+        
+    def set_saldo(self, saldo):
+        self.__saldo = saldo
+    
+    def get_saldo(self):
+        return self.__saldo        
+        
+class produkt():
+    pass
+
 def api_products():
     url = "https://dummyjson.com/products"
     response = requests.get(url)
@@ -8,7 +38,7 @@ def api_products():
         data = response.json()
         products = data.get("products", [])
         
-        for product in products[5:]:  # visa antal produkter
+        for product in products[:5]:  # visa antal produkter
             print(f"Namn: {product['title']}")
             print(f"Pris: {product['price']} USD")
             print(f"Beskrivning: {product['description']}")
